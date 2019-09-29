24adf102b3f1a39526946cb583fa942134061108
r-i-c-o <ricorico13200@gmail.com>
2019-09-29 14:11:34 +0300

    updated word class

--- app/src/main/java/com/ricode/kotlinwords/packs/Word.kt
+++ app/src/main/java/com/ricode/kotlinwords/packs/Word.kt
@@ -1,9 +1,9 @@
 package com.ricode.kotlinwords.packs
 
 class Word(
-    val title: String?,
-    val transcription: String?,
-    val translation: String?
+    val title: String? = "title",
+    val transcription: String? = "transcription",
+    val translation: String? = "translation"
 ){
     var tries: Int = 2
 }
