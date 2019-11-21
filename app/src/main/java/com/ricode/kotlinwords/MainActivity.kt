2dc01f628c7d37a14893c5bf0c39fa069aa88b4f
r-i-c-o <ricorico13200@gmail.com>
2019-11-21 23:03:40 +0300

    removed useless toolbar, updated button styles

--- app/src/main/java/com/ricode/kotlinwords/MainActivity.kt
+++ app/src/main/java/com/ricode/kotlinwords/MainActivity.kt
@@ -2,9 +2,7 @@ package com.ricode.kotlinwords
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        /*val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val navController = findNavController(R.id.fragment)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController)*/
 
         MobileAds.initialize(this) {}
     }
